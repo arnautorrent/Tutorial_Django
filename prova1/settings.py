9e00101f78ee57a18505385940eb810c0c22e145
arnau <nauix26@gmail.com>
2015-02-09 23:00:37 +0100

    MySQL support

--- prova1/settings.py
+++ prova1/settings.py
@@ -59,8 +59,12 @@ WSGI_APPLICATION = 'prova1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'proves',
+        'USER': 'root',
+        'PASSWORD': '12345',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
